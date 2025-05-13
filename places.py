a1196a005ba3cab39e29adddae24072fa6d35bd4
GeorgiiKrass <landing.kga@gmail.com>
2025-05-13 23:20:16 +0300

    Update places.py

--- places.py
+++ places.py
@@ -15,12 +15,10 @@ CENTER_LAT, CENTER_LON = 46.4825, 30.7233  # Центр Одеси
 RADIUS = 3000
 
 def get_random_places(n=3):
-    places = []
+    all_places = []
     used_ids = set()
-    tries = 0
 
-    while len(places) < n and tries < 20:
-        place_type = random.choice(ALLOWED_TYPES)
+    for place_type in ALLOWED_TYPES:
         response = requests.get(
             "https://maps.googleapis.com/maps/api/place/nearbysearch/json",
             params={
@@ -32,7 +30,6 @@ def get_random_places(n=3):
         )
         data = response.json()
         candidates = data.get("results", [])
-        random.shuffle(candidates)
 
         for place in candidates:
             place_id = place["place_id"]
@@ -44,12 +41,10 @@ def get_random_places(n=3):
             lon = place["geometry"]["location"]["lng"]
             url = f"https://maps.google.com/?q={lat},{lon}"
 
-            places.append({"name": name, "lat": lat, "lon": lon, "url": url})
+            all_places.append({"name": name, "lat": lat, "lon": lon, "url": url})
             used_ids.add(place_id)
 
-            if len(places) == n:
-                break
+    if len(all_places) < n:
+        return []
 
-        tries += 1
-
-    return places
+    return random.sample(all_places, n)
